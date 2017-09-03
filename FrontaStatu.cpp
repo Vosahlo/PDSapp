35c74aad4d17ad87b38f1d5ccf6d174777e64c0d
qwerty2586 <qwerty2@students.zcu.cz>
2017-09-03 13:23:29 +0200

    fronta statu

---
+++ FrontaStatu.cpp
@@ -0,0 +1,62 @@
+//
+// Created by qwerty on 3. 9. 2017.
+//
+
+#include "FrontaStatu.h"
+
+FrontaStatu::FrontaStatu() {
+    items = nullptr;
+}
+
+void FrontaStatu::addSpeech(int index) {
+    FrontaItem *item = new FrontaItem();
+    item -> stat = &staty[index];
+    FrontaItem *pom = items;
+    if (!items) {
+        items = item;
+        active = 0;
+    } else {
+        while (pom->dalsi) pom = pom->dalsi;
+        pom->dalsi = item;
+    }
+}
+
+void FrontaStatu::addRSpeech(int index) {
+    FrontaItem *item = new FrontaItem();
+    item -> stat = &staty[index];
+    FrontaItem *pom = items;
+    if (!items) {
+        items = item;
+    } else {
+        //while (pom->dalsi) pom = pom->dalsi;
+        while (pom->reakce) pom = pom->reakce;
+        pom->reakce = item;
+    }
+}
+
+void FrontaStatu::next() {
+    if (!items)
+        return;
+    active++;
+    FrontaItem *pom = items;
+    bool smazat = false;
+    for (int i = 0; i < active; ++i) {
+        if (!pom->reakce) {
+            smazat = true;
+            break;
+        }
+        pom = pom->reakce;
+    }
+    if (smazat) {
+
+        while (items->reakce) {
+            pom = items->reakce;
+            items->reakce = items->reakce->reakce;
+            delete pom;
+        }
+        pom = items;
+        items = items->dalsi;
+        delete pom;
+        active = (items) ? 0 : -1;
+    }
+}
---
+++ FrontaStatu.h
@@ -0,0 +1,37 @@
+//
+// Created by qwerty on 3. 9. 2017.
+//
+
+#ifndef PDSAPP_FRONTASTATU_H
+#define PDSAPP_FRONTASTATU_H
+
+
+#include <QObject>
+#include "Stat.h"
+
+class FrontaStatu : public QObject {
+    Q_OBJECT
+public:
+    explicit FrontaStatu();
+    int get_active() {return this->active; };
+    FrontaItem *get_items() { return this->items; };
+
+    struct FrontaItem {
+        FrontaItem *dalsi = nullptr;
+        FrontaItem *reakce = nullptr;
+        Stat *stat = nullptr;
+    };
+
+public slots:
+    void addSpeech(int index);
+    void addRSpeech(int index);
+    void next();
+
+private:
+
+    FrontaItem *items;
+    int active = -1;
+};
+
+
+#endif //PDSAPP_FRONTASTATU_H
